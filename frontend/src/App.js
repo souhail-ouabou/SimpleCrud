27f81a0a77fe455eab0519b34b4f665da321e730
unknown <souhail7ouabou@gmail.com>
2023-04-03 04:47:17 +0000

    fnish2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
-import { Button, Card, Navbar } from 'flowbite-react'
 import { BrowserRouter } from 'react-router-dom'
 import { Route, Routes } from 'react-router-dom/dist'
-import Article from './components/Article'
 import Login from './components/Login'
 import Nav from './components/Nav'
 import { dispatchToken } from './redux/actions/tokenAction'
@@ -9,20 +7,14 @@ import { useDispatch, useSelector } from 'react-redux'
 import {
     dispatchLogin,
     dispatchGetUser,
-    fetchUser,
 } from './redux/actions/authAction'
 import { useEffect } from 'react'
-import { ToastContainer } from 'react-toastify'
 import Home from './components/Home'
 import FormArticle from './components/FormArticle'
 import EditArticle from './components/EditArticle'
-import ArticleDetails from './components/ArticleDetails'
 
 function App() {
-    const getUserReducer = useSelector((state) => state.getUserReducer)
-    const { user, isAdmin, isLogged: isLoggedgetuserReducer } = getUserReducer
     const auth = useSelector((state) => state.auth)
-    const { userInfo, isLogged } = auth
     const token = useSelector((state) => state.token)
     const dispatch = useDispatch()
     useEffect(() => {
@@ -47,7 +39,6 @@ function App() {
                     <Route path="/*" element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/add" element={<FormArticle />} />
-                    <Route path="/details/:id" element={<ArticleDetails />} />
                     <Route path="/edit/:id" element={<EditArticle />} />
                 </Routes>
             </BrowserRouter>
